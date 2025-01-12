b3e74e28c67f5b2ffa52d0609adefa239d8c2e0b
Italo-omni <italo_lopes_araujo@hotmail.com>
2025-01-12 16:54:49 -0300

    floating-cta

--- src/components/FloatingCTA.tsx
+++ src/components/FloatingCTA.tsx
@@ -32,7 +32,7 @@ export const FloatingCTA = () => {
         onClick={handleClick}
         className="cta-button inline-flex items-center gap-2"
       >
-        <ShoppingCart className="w-1 h-5" />
+        <ShoppingCart className="w-5 h-5" />
         Comprar Agora por R$ 197,00
       </button>
     </div>
