25edbf440bd1f73a19fe607136e4af1edb6285e0
Italo-omni <italo_lopes_araujo@hotmail.com>
2025-01-13 15:53:01 -0300

    conforto para suas costas

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -15,8 +15,8 @@ export const HeroSection = () => {
             </div>
             
             <h1 className="font-heading text-3xl sm:text-4xl md:text-5xl font-bold text-gray-900 mb-4">
-            liberte-se do <br />
-              <span className="text-primary">desconforto nas costas!</span><br />
+            Descubra como trazer mais <br />
+              <span className="text-primary">conforto para suas costas</span><br />
               com o Corretor Postural ActiveFit™
             </h1>
             
